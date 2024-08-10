0759cf717f2303d926c9a09acaf1253c99fbc0ef
Yeonwoo.Yoo <zerial117@gmail.com>
2024-08-10 23:17:05 +0900

    Practice_in_VSC

---
+++ Project_2/Django_8.9/todoproject/todoapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Task, List
+
+class TaskForm(forms.ModelForm):
+    list = forms.ModelChoiceField(queryset=List.objects.all(), empty_label=None)    
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'list']
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
