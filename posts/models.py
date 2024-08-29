8f6aa83bee5e06b1a9657bbfe5e5670085b76c3a
Yeon Woo <zerial117@gmail.com>
2024-08-29 09:34:04 +0900

    posts models 설계

--- posts/models.py
+++ posts/models.py
@@ -1,1 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.Foreignkey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tags = TaggableManager()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
