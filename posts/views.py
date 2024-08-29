68086adfdd305711d4932d508b2a12bd8a79423a
Yeon Woo <zerial117@gmail.com>
2024-08-29 15:08:35 +0900

    posts.views 수정 및 초안 업데이트

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+from taggit.models import Tag
 from .models import Post, Category
 from .forms import PostForm
 
@@ -62,20 +63,44 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
     def handle_no_permission(self):
         return redirect('post_detail', pk=self.get_object().pk)
 
-class CategorypostsView(ListView):
+class CategoryPostsView(ListView):
     model = Post
-    template_name = 'blog/category_posts.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    paginate_by = 10
 
     def get_queryset(self):
-        return Post.objects.filter(category__slug=self.kwargs)
-    
-class CategoryPostsView():
-    pass
+        category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Post.objects.filter(category=category).order_by('-created_at')
 
-class TagPostsView():
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
 
-class SearchPostsView():
-    pass
+class TagPostsView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        return Post.objects.filter(tags__in=[tag]).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        return context
+
+class SearchPostsView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Post.objects.filter(title__icontains=query).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q')
+        return context
