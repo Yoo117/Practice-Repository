6602aea6ed20eecae81f37d4c05a42994295734e
Yeon Woo <zerial117@gmail.com>
2024-08-29 17:20:32 +0900

    accounts, interactions, posts 앱 admin 설정 및 에러 제거

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    ordering = ('-date_joined',)
+    list_display = ('email', 'username', 'nickname', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    search_fields = ('email', 'username', 'nickname')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'nickname')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'nickname', 'password1', 'password2'),
+        }),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj:
+            kwargs['exclude'] = ('password',)
+        return super().get_form(request, obj, **kwargs)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at', 'updated_at', 'views')
+    list_filter = ('category', 'created_at', 'author', 'tags')
+    search_fields = ('title', 'content', 'author__username', 'tags__name')
+    readonly_fields = ('created_at', 'updated_at', 'views')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (None, {'fields': ('title', 'content', 'author', 'category', 'tags', 'image', 'views')}),
+        ('Dates', {'fields': ('created_at', 'updated_at')}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        # Optionally, handle additional processing before saving
+        super().save_model(request, obj, form, change)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    ordering = ('name',)
