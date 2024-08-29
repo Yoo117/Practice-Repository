3d96083c87ea1b306be4adb2b69fffb64f44c5c8
Yeon Woo <zerial117@gmail.com>
2024-08-29 23:56:11 +0900

    interactions 앱 forms 및 views 업데이트

---
+++ interactions/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Comment, Bookmark, Notification
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class ReplyForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class BookmarkForm(forms.ModelForm):
+    class Meta:
+        model = Bookmark
+        fields = []  # URL의 post_id를 사용하므로 필드가 필요하지 않습니다.
+
+class NotificationForm(forms.ModelForm):
+    class Meta:
+        model = Notification
+        fields = ['is_read']
