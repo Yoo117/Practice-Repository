9bff82c5c13196fc259715e695e23747f0363f93
Yeon Woo <zerial117@gmail.com>
2024-08-30 14:25:11 +0900

    form에 tag widget 추가

--- posts/forms.py
+++ posts/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+from taggit.forms import TagWidget
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        widget=TagWidget(),  
+        required=False
+    )
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'category', 'tags', 'image']
-        widgets = {
-            'tags': forms.TextInput(attrs={'data-role': 'tagsinput'}),
-        }
